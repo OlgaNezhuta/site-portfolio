6c7196052474bef8d51eb15623414293a2c0ac3c
OlgaNezhuta <vzdya@yandex.ru>
2016-06-30 19:41:12 +0300

    problems with mixin

--- gulp/paths/template.js
+++ gulp/paths/template.js
@@ -1,5 +1,9 @@
 'use strict';
 
 module.exports = [
-  './source/template/index.jade'
+    './source/template/index.jade',
+    './source/template/pages/welcome-page.jade',
+    './source/template/pages/about-page.jade',
+    './source/template/pages/blog-page.jade',
+    './source/template/pages/works-page.jade'
 ];
